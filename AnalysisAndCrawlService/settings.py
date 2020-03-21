e3148b91d8d4cdbfd804c3a94890c5a5057152e9
Fibonacci998 <fibonacci998@gmail.com>
2020-03-21 19:32:42 +0700

    update

--- AnalysisAndCrawlService/settings.py
+++ AnalysisAndCrawlService/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xp4$f(-qg)p0su%gl307ql+3&ep4p*zsf@4)ig3%96f2p_hspr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -84,13 +84,22 @@ WSGI_APPLICATION = 'AnalysisAndCrawlService.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'AnalysisAndCrawlDB',
+        'USER': 'postgres',
+        'PASSWORD': 'homersimpson',
+        'HOST': 'database-1.c4dxodf0guqt.ap-southeast-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
